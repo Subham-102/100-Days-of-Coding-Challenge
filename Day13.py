9c7ec9f6ad745de4e6cb363c24d4ef011c874bb2
Subham Shaw <subhamkrshawa21s@gmail.com>
2024-04-01 18:27:15 +0530

    Add files via upload

---
+++ Day13.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        num_indices = {}
+        for i, num in enumerate(numbers):
+            com = target - num
+            if com in num_indices:
+                return [num_indices[com] + 1, i + 1]
+            num_indices[num] = i
+        return []
+                    
+        
+        
