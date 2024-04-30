6a9c16fdf84f609d24a791278d0b521becb02633
Subham Shaw <subhamkrshawa21s@gmail.com>
2024-04-30 13:58:30 +0530

    Add files via upload

---
+++ Day42.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        
+        int n = stones.size();
+        priority_queue<int> pq;
+        for(auto x : stones)
+        {
+            pq.push(x);
+        }
+        while(pq.size() > 1)
+        {
+            int first = pq.top();
+            
+            pq.pop();
+            
+            int second = pq.top();
+            
+            pq.pop();
+            
+            if(first != second)
+            {
+                pq.push(abs(first - second));
+            }
+        }
+        
+        return pq.empty() ? 0 : pq.top();
+    }
+};
