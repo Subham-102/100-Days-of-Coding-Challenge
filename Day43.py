075c317221ecdd8e3015644d8464a1e0ec40fe29
Subham Shaw <subhamkrshawa21s@gmail.com>
2024-05-01 19:11:58 +0530

    Add files via upload

---
+++ Day43.py
@@ -0,0 +1,12 @@
+
+class Solution(object):
+    def pivotIndex(self, nums):
+        leftSum, rightSum = 0, sum(nums)
+        
+        for idx, ele in enumerate(nums):
+            rightSum -= ele
+         
+            if leftSum == rightSum:
+                return idx    
+            leftSum += ele
+        return -1     
