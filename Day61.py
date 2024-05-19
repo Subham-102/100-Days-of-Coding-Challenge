3daf4a6402379ed059bf600adf9c1653b7dcbb89
Subham Shaw <subhamkrshawa21s@gmail.com>
2024-05-19 22:34:55 +0530

    Add files via upload

---
+++ Day61.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def isPalindrome(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        slow = fast = head 
+        while fast and fast.next:
+           fast = fast.next.next
+           slow = slow.next 
+            
+        stack1=[]
+        stack2 = []
+        
+        fast = slow 
+        slow = head
+        while fast:
+            stack2.append(slow.val)
+            stack1.append(fast.val)
+            fast = fast.next
+            slow = slow.next 
+            
+        stack2.reverse()
+        return stack1 == stack2
+        
