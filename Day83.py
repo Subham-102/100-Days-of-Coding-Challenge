72fb5274463891dcc998d0b4d4692feb7def547f
Subham Shaw <subhamkrshawa21s@gmail.com>
2024-06-11 19:12:57 +0530

    Add files via upload

---
+++ Day83.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def zigzagLevelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if not root:
+            return []
+        b=[root]
+        res=[]
+        flag=True
+        while b:
+            val=[]
+            b1=[]
+            for i in b:
+                val.append(i.val)
+                if i.left:
+                    b1.append(i.left)
+                if i.right:
+                    b1.append(i.right)
+            if flag:
+                res.append(val)
+                flag=False
+            else:
+                res.append(val[::-1])
+                flag=True
+            b=b1
+        return res
+
